b2c6f108d393c1609d56759f3ac8a5a2f6936894
sdgamboa <samuel.gamboa.tuz@gmail.com>
2024-12-27 00:23:23 -0500

    Add shinydashbordPlus to imports

--- R/boxes.R
+++ R/boxes.R
@@ -1,27 +1,27 @@
 boxDiscreteShort <- function(x, dat) {
     plotOutputId <- make.names(paste0(x, "_plot"))
     widgetId <- make.names(paste0(x, "_menu"))
-        shinydashboard::box(
-            width = 4,
-            title = .textR(x),
-            solidHeader = TRUE,
-            class = "fixed-height-box",
-                shiny::plotOutput(
-                    plotOutputId
-                ),
-                shinyWidgets::pickerInput(
-                    inputId = widgetId,
-                    label = NULL,
-                    choices = sort(unique(dat[[x]])),
-                    multiple = TRUE,
-                    selected = sort(unique(dat[[x]])),
-                    options = list(
-                        `actions-box` = TRUE,
-                        `live-search` = FALSE,
-                        `selected-text-format` = "count > 1"
-                    )
-                )
+    shinydashboard::box(
+        width = 4,
+        title = .textR(x),
+        solidHeader = TRUE,
+        # class = "fixed-height-box",
+        shiny::plotOutput(
+            plotOutputId
+        ),
+        shinyWidgets::pickerInput(
+            inputId = widgetId,
+            label = NULL,
+            choices = sort(unique(dat[[x]])),
+            multiple = TRUE,
+            selected = sort(unique(dat[[x]])),
+            options = list(
+                `actions-box` = TRUE,
+                `live-search` = FALSE,
+                `selected-text-format` = "count > 1"
+            )
         )
+    )
 }
 
 # boxDiscreteLong <- function(x) {
@@ -41,21 +41,21 @@ boxDiscreteShort <- function(x, dat) {
 boxContinuous <- function(x, dat) {
     plotOutputId <- make.names(paste0(x, "_plot"))
     widgetId <- make.names(paste0(x, "_range"))
-        shinydashboard::box(
-            width = 4,
-            title = .textR(x),
-            solidHeader = TRUE,
-            class = "fixed-height-box",
-                shiny::plotOutput(plotOutputId),
-                shiny::sliderInput(
-                    inputId = widgetId,
-                    label = NULL,
-                    min = floor(min(dat[[x]], na.rm = TRUE)),
-                    max = ceiling(max(dat[[x]], na.rm = TRUE)),
-                    value = c(floor(min(dat[[x]], na.rm = TRUE)), ceiling(max(dat[[x]], na.rm = TRUE))),
-                    step = 1
-                )
+    shinydashboard::box(
+        width = 4,
+        title = .textR(x),
+        solidHeader = TRUE,
+        # class = "fixed-height-box",
+        shiny::plotOutput(plotOutputId),
+        shiny::sliderInput(
+            inputId = widgetId,
+            label = NULL,
+            min = floor(min(dat[[x]], na.rm = TRUE)),
+            max = ceiling(max(dat[[x]], na.rm = TRUE)),
+            value = c(floor(min(dat[[x]], na.rm = TRUE)), ceiling(max(dat[[x]], na.rm = TRUE))),
+            step = 1
         )
+    )
 }
 
 boxFun <- function(x, dat) {
